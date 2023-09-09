88b2507d91d9cc48a59d67000169b94749a39029
Zackry Langford <zackrylangford@gmail.com>
2023-09-08 21:27:38 -0400

    played around with countdown

--- swift-files/countdown.swift
+++ swift-files/countdown.swift
@@ -13,3 +13,39 @@ while count >= 0 {
 
 print("Blast off!")
 
+//using timer module
+
+import Foundation
+
+var count = 60
+
+let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+    print("\(count) seconds.")
+    count -= 1
+    if count == 0 {
+        timer.invalidate()
+        print("Blast off!")
+    }
+}
+
+
+
+timer.invalidate()
+
+let hardness = sender.currentTitle!
+
+secondsRemaining = eggTimes[hardness]!
+
+timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+
+
+@objc func updateTimer() {
+    if secondsRemaining > 0 {
+        print("\(secondsRemaining) seconds.")
+        secondsRemaining -= 1
+    } else {
+        timer.invalidate()
+        print("Blast off!")
+    }
+}
+
